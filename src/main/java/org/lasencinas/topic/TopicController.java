57e4e280522d555285b1806230a0938d4b710dbe
alexnro <alexjandron99@gmail.com>
2019-05-06 21:38:38 +0200

    Añadido Topic y cambiado getAllTopics para que devuelva JSON

--- src/main/java/org/lasencinas/topic/TopicController.java
+++ src/main/java/org/lasencinas/topic/TopicController.java
@@ -3,11 +3,18 @@ package org.lasencinas.topic;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+
 @RestController
 public class TopicController {
 
     @RequestMapping(path="/topic")
-    public String getAllTopics() {
-        return "All Topics";
+    public List<Topic> getAllTopics() {
+        return Arrays.asList(
+                new Topic("Spring", "Spring Frameword", "Spring Framework Description"),
+                new Topic("java", "Core Java", "Core Java Description"),
+                new Topic("javascript", "JavaScript", "JavaScript Description")
+        );
     }
 }
